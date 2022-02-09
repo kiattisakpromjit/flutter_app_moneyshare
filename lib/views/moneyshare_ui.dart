fe73679f7452c52e3f9c42f711b662e98851f30b
kiattisakpromjit <s6335n10015@sau.ac.th>
2022-02-09 12:29:54 +0700

    Code V.4 XXXXXXXXXX

--- lib/views/moneyshare_ui.dart
+++ lib/views/moneyshare_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MoneyshareUI extends StatefulWidget {
   const MoneyshareUI({ Key? key }) : super(key: key);
@@ -8,6 +9,63 @@ class MoneyshareUI extends StatefulWidget {
 }
 
 class _MoneyshareUIState extends State<MoneyshareUI> {
+  bool? check_tip = false ;
+
+  TextEditingController? money_ctrl = TextEditingController();
+  TextEditingController? person_ctrl = TextEditingController();
+  TextEditingController? tip_ctrl = TextEditingController();
+  //เมธอด -> โค้ดแสดง Dialog เตือน โดยจะรับข้อความมาแสดงที่ Dialog
+
+  showWarningDialog(context, msg) {
+
+    //เรียกใช้งานฟังก์ชัน showDialog
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Container(
+            color: Colors.deepPurple,
+            padding: EdgeInsets.only(
+              top: 10.0,
+              bottom: 10.0,
+            ),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                'คำเตือน',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          content: Text(
+            msg,
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'ตกลง',
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurple,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,6 +95,7 @@ class _MoneyshareUIState extends State<MoneyshareUI> {
                   top: 60.0,
                 ),
                 child: TextField(
+                  controller: money_ctrl,
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     focusedBorder: UnderlineInputBorder(
@@ -54,11 +113,182 @@ class _MoneyshareUIState extends State<MoneyshareUI> {
                       color: Colors.grey,
                     ),
                     prefixIcon: Icon(
-                      Icons.money_off_csred_sharp,
+                      FontAwesomeIcons.moneyBillWave,
                       color: Color(0xFF7F0CAE),
                     ),
                   ),
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40.0,
+                  right: 40.0,
+                  top: 30.0,
+                ),
+                child: TextField(
+                  controller: person_ctrl,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFF7F0CAE),
+                      ),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFF7F0CAE),
+                      ),
+                    ),
+                    hintText: 'ป้อนจำนวนคน (คน)',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Color(0xFF7F0CAE),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Checkbox(
+                    onChanged: (val){
+                      setState(() {
+                        check_tip = val ;
+                      });
+                    },
+                    value: check_tip,
+                    activeColor: Color(0xFF7F0CAE),
+                    checkColor: Colors.yellow,
+                    side: BorderSide(
+                      color: Color(0xFF7F0CAE),
+                    ),
+                  ),
+                  Text(
+                    'ทิปให้พนักงานเสริฟ',
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40.0,
+                  right: 40.0,
+                ),
+                child: TextField(
+                  controller: tip_ctrl,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFF7F0CAE),
+                      ),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFF7F0CAE),
+                      ),
+                    ),
+                    hintText: 'ป้อนจำนวนเงินทิป (บาท)',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.coins,
+                      color: Color(0xFF7F0CAE),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  if(money_ctrl!.text.length == 0){ // . length นับจำนวนตัวอักษร
+                    //แสดง msg เตือน
+                    showWarningDialog(context, 'ป้อนจำนวนเงินด้วย');
+                    return;
+                  }else if(person_ctrl!.text.length == 0){
+                    //แสดง msg เตือน
+                    showWarningDialog(context, 'ป้อนจำนวนคนด้วย');
+                    return;
+                  }else if(check_tip == true){
+                    if(tip_ctrl!.text.length == 0){
+                      //แสดง msg เตือน  
+                    showWarningDialog(context, 'ป้อนจำนวนทิปด้วย');
+                    return;
+                    }else{
+                      //คำนวณ (เงิน+ทิป)/คน
+                    }
+                  }else{
+                    //คำนวณ เงิน/คน
+                  }
+                },
+                child: Text(
+                  'คำนวณ',
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width -  80.0,
+                    50.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      50.0,
+                    ),
+                  ),
+                  primary: Color(0xFF7F0CAE),
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    money_ctrl!.text = '';
+                    person_ctrl!.text = '';
+                    tip_ctrl!.text = '';
+                    check_tip = false;
+                  });
+                },
+                icon: Icon(
+                  Icons.refresh,
+                ),
+                label: Text(
+                  'ยกเลิก'
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width - 80.0,
+                    50.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      50.0,
+                    ),
+                  ),
+                  primary: Colors.red,
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Text(
+                'Created by BC SAU 2022',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[700],
+                  fontSize: 16.0,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
               ),
             ],
           ),
